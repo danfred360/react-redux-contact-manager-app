2823a967f7d6cdb2efedc61091168763bf3ae39d
danfred360 <dannyfrederick360@gmail.com>
2019-02-27 12:54:49 -0500

    init

---
+++ build/precache-manifest.f48b30c96a6d0bca1fd468eb6a0b6517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fc16e7287ce26f3c7a9c",
+    "url": "/static/js/main.fc16e728.chunk.js"
+  },
+  {
+    "revision": "7eec6c45c96e55e3e173",
+    "url": "/static/js/2.7eec6c45.chunk.js"
+  },
+  {
+    "revision": "fc16e7287ce26f3c7a9c",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "7eec6c45c96e55e3e173",
+    "url": "/static/css/2.b7485962.chunk.css"
+  },
+  {
+    "revision": "8a8fc4360c4b99a63c565258ff8b2e7a",
+    "url": "/index.html"
+  }
+];
